0fd85d2dc5e2d7f726acdee9e880fcb4f24c18b7
Jonathan Herández Zapata <nahtanoj.hz@gmail.com>
2019-09-22 20:52:40 -0500

    i18n pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Inject, LOCALE_ID } from '@angular/core';
-import { HeroesService } from './heroes.service';
 import { Observable } from 'rxjs';
+import { registerLocaleData, getCurrencySymbol } from '@angular/common';
+import localeEn from '@angular/common/locales/en';
+import localeEs from '@angular/common/locales/es-CO';
+
+import { HeroesService } from './heroes.service';
 import { IHero } from './heroes.interface';
 
 @Component({
@@ -15,11 +19,33 @@ export class AppComponent implements OnInit {
     { code: 'en', label: 'English' },
     { code: 'es', label: 'Español' }
   ];
+  today: number = Date.now();
+  currencySymbol: string;
+  value: number = 2000.3495;
 
-  constructor(private heroesService: HeroesService, @Inject(LOCALE_ID) protected localeId: string) {
+  constructor( private heroesService: HeroesService, @Inject(LOCALE_ID) protected localeId: string ) {
+    this.setLocale(localeId);
   }
 
   ngOnInit() {
     this.heroes$ = <Observable<IHero[]>>this.heroesService.getHeroes();
+  }
+
+  setLocale(localeId: string) {
+    switch (localeId) {
+      case 'es':
+        registerLocaleData(localeEs, localeId);
+        this.currencySymbol = 'COP';
+        break;
+      
+      case 'en-US':
+        registerLocaleData(localeEs, 'en');
+        this.currencySymbol = 'USD';
+        break;
+        
+        default:
+          break;
+    }
+    
   }
 }
