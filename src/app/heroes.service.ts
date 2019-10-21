9490a3a5494cd08f0ef4172fb329b50ba4b46b9d
Sebastian Sierra <sebastian.sierra@e-hps.com>
2019-10-21 18:20:47 -0500

    Cleaning code

--- src/app/heroes.service.ts
+++ src/app/heroes.service.ts
@@ -11,7 +11,7 @@ export class HeroesService {
   constructor(private httpClient: HttpClient) { }
 
   getHeroes(): Observable<any | IHero[]> {
-    const url  = 'https://gist.githubusercontent.com/sierra002/f0b10026d986f3de3f8d36783c8f3314/raw/5c9125097717ff20076b0ddf767167bab5e72a3e/gistfile1.txt';
+    const url  = 'https://gist.githubusercontent.com/sierra002/f0b10026d986f3de3f8d36783c8f3314/raw/ae03f62036885ba2db37837434840d30742367bc/gistfile1.txt';
     return this.httpClient.get(url);
   }
 }
